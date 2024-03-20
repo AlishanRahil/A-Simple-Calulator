96f83c55786a68b6141b381c96f8f3c2000e6bfb
Alishan Rahil <160777070+AlishanRahil@users.noreply.github.com>
2024-03-21 01:35:39 +0500

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,28 @@
+// #!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter Your Number", type: "number", name: "firstnumber" },
+    { message: "Enter Your Second Number", type: "number", name: "secondnumber" },
+    {
+        message: "Select Any Opreator Which You Want To Do",
+        type: "list",
+        name: "Opreators",
+        choices: ["Addition", "Substractiobn", "Multiplication", "Division"]
+    }
+]);
+if (answer.Opreators === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.Opreators === "Substractiobn") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.Opreators === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else if (answer.Opreators === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else {
+    console.log("please Inter Valid Number");
+}
+;
